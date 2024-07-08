e3df23cf573ce049f77d56fdfd6afd5d3bcd35c6
Rishabh Sharma <rishabh.jangra04@gmail.com>
2024-07-08 17:51:07 +0530

    Auto-generated commit

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-CPZJITZJ.mjs";import"./chunk-MUAWY7E7.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-EU7AG42S.mjs";import"./chunk-7EWVBSQ5.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
